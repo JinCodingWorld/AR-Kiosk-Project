070739665b9e00da555c63a9737108f16694a6e2
Jiho Lee <ijiho5826@gmail.com>
2024-09-19 08:42:51 +0900

    Update GoogleTranslation.cs
    api key deleted

--- Assets/Scripts/GoogleTranslation.cs
+++ Assets/Scripts/GoogleTranslation.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class GoogleTranslation : MonoBehaviour
 {
-    private string apiKey = "AIzaSyC54yBM_zkDNQc8-_ZlaPJjr1JN8mqIZsA"; // Replace with your actual Translation API key
+    private string apiKey = "  "; // Replace with your actual Translation API key
     private string url = "https://translation.googleapis.com/language/translate/v2?key=";
     public Text translatedText; // UI Text to display the translated text
 
